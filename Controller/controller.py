1a10d7e763f2bae395178d15504caa2b475a7042
jabbi <jabbi@openmailbox.org>
2018-01-03 14:33:15 +0100

    + check if valid key

--- Controller/controller.py
+++ Controller/controller.py
@@ -8,6 +8,13 @@ key_mapping = {
     1:"B"       # Left      # Back
                 }
 subscriber = []
+
+def in_key_mapping(key:int):
+    for i in key_mapping:
+        if i == key:
+            return True
+    return False
+
 def main():
     selector = DefaultSelector()
     devices = [InputDevice(fn) for fn in list_devices()]
@@ -21,17 +28,22 @@ def main():
 
     while True:
         for key, mask in selector.select():
+            print("[1]event" + str(key) + " "+ str(mask))
             device = key.fileobj
             for event in device.read():
-                if event.type == ecodes.EV_KEY :
-                    print(str(key.fd)+" " +str(event.code) + " " + str(mask))
-                    for m in subscriber:
-                        m(event.code, key.fd)
+                if event.type == ecodes.EV_KEY:
+                    if categorize(event).keystate == 1 and not in_key_mapping(event.code):
+                        for m in subscriber:
+                            print("Event geschmießen")
+                            m(event.code, key.fd)
+
+
 
 def subscribe(method):
     """This method should get a method like this method(button : int, by: int) if this not the case, your method will not be added"""
     global subscriber
     subscriber.append(method)
+
 
 def unsubscribe(method):
     global subscriber
