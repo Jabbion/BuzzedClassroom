a0a7ce1367585c25e88564f784a5d1f78d4e3222
jabbi <jabbi@openmailbox.org>
2018-01-08 20:11:12 +0100

    +add Threading

--- Controller/controller.py
+++ Controller/controller.py
@@ -1,5 +1,6 @@
 from evdev import InputDevice, list_devices, categorize, ecodes
 from selectors import DefaultSelector, EVENT_READ
+from threading import Thread
 
 key_mapping = {
     115:"C",    # Up
@@ -35,9 +36,7 @@ def main():
                     if cat.keystate == 1 and in_key_mapping(event.code):
                         for m in subscriber:
                             print("Event geschmissen")
-                            m(event.code, key.fd)
-
-
+                            Thread(target=m, args=(event.code, key.fd)).start()
 
 def subscribe(method):
     """This method should get a method like this method(button : int, by: int) if this not the case, your method will not be added"""
