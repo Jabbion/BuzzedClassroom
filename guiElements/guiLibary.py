a8ad2cf88e9a2e664cd1721bb9b320e4888cce98
TheDoctor <doctorshacking@gmail.com>
2018-01-20 16:01:13 +0100

    + Switched in quizzes_overview text_list to top_down_text_list

--- guiElements/guiLibary.py
+++ guiElements/guiLibary.py
@@ -2,18 +2,21 @@ from guiElements.main_window import MainWindows
 import pygame
 BLACK = (0, 0, 0)
 
+# <Settings>
+topDownDiffList = 80
+fontSizeSelectedList = 80
+fontSizeUnselectedList = 50
+visibleNumPerPage = 7
 
-def text_box(msg, x, y, weidth, height, surface, font_size=75, color=BLACK):
+def text_box(msg, x, y, width, height, surface, font_size=75, color=BLACK):
     small_text = pygame.font.SysFont("LikhanNormal", font_size)
     text_surf, text_rect = text_objects(msg, small_text, color=color)
-    text_rect.center = ((x + (weidth / 2)), (y + (height / 2)))
+    text_rect.center = ((x + (width / 2)), (y + (height / 2)))
     surface.blit(text_surf, text_rect)
-
 
 def text_objects(text, font, color=BLACK):
     text_surface = font.render(text, True, BLACK)
     return text_surface, text_surface.get_rect()
-
 
 def text_list(names, selected = None):
     temp_display = MainWindows.get_new_background('images/winner.png')
@@ -26,5 +29,45 @@ def text_list(names, selected = None):
                      , font_size=50, color=(200, 200, 200)) #old 100
         else:
             text_box(name, (spalte * 300) + 100, (int(zeile) * 50) + 200, 100, 100, temp_display, font_size=25) #old 50
+
+    return temp_display
+
+def top_down_text_list(names, selected = None):
+    bHighlight = True
+    if selected == None:
+        selected = 0
+        bHighlight = False
+
+    visibleNames = []
+    lenNames = len(names)
+    bScrollable = False
+
+    if selected < visibleNumPerPage:
+        if lenNames < visibleNumPerPage:
+            visibleNames = names[0:lenNames]
+        else:
+            visibleNames = names[0:visibleNumPerPage]
+
+            if visibleNumPerPage < lenNames:
+                bScrollable = True
+
+    elif selected >= visibleNumPerPage:
+        if selected < lenNames - 1:
+            bScrollable = True
+
+        visibleNames = names[selected - visibleNumPerPage + 1: selected + 1]
+        selected = visibleNumPerPage - 1
+
+    temp_display = MainWindows.get_new_background("images/winner.png")
+    xMax, yMax = temp_display.get_size()
+
+    for i, name in enumerate(visibleNames):
+        if i == selected and bHighlight == True:
+            text_box("[ " + name + " ]", xMax / 2 - 40, i * topDownDiffList + 150, 100, 100, temp_display, font_size = fontSizeSelectedList, color = (200, 200, 200))
+        else:
+            text_box(name, xMax / 2 - 40, i * topDownDiffList + 150, 100, 100, temp_display, font_size=fontSizeUnselectedList)
+
+    if bScrollable == True:
+        text_box("...", xMax / 2 - 40, visibleNumPerPage * topDownDiffList + 150, 100, 100, temp_display, font_size=fontSizeUnselectedList)
 
     return temp_display
--- guiElements/quizzes_overview.py
+++ guiElements/quizzes_overview.py
@@ -1,7 +1,7 @@
 from guiElements.player_overview import player_overview
-from guiElements.guiLibary import text_box, text_list
+from guiElements.guiLibary import text_box, top_down_text_list
 
 def quizzes_overview(quiz: list, selected=None):
-    img = text_list(quiz, selected)
+    img = top_down_text_list(quiz, selected)
     text_box("Quiz", 800 - 50, 30, 100, 100, img, font_size=100)
     return img
